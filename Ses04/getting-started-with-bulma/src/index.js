a4611406d8e5ff0662789d59f98a5df07088bdd5
Leeoness <salvatoredione@gmail.com>
2020-07-11 16:09:17 -0500

    Crear el entry a traves del js y su scss de bulma

--- Ses04/getting-started-with-bulma/src/index.js
+++ Ses04/getting-started-with-bulma/src/index.js
@@ -1,5 +1,7 @@
 //require ('./sass/styles.scss')
+import './sass/mybulma.scss'
 import './sass/styles.scss'
 import '@fortawesome/fontawesome-free/js/all'
+
 const sayHello = 'Getting started with Bulma'
 console.log(sayHello)
