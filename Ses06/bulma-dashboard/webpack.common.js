42b01f7356fdb9b1173250202efae072ca444452
Leeoness <salvatoredione@gmail.com>
2020-07-25 16:08:08 -0500

    archivos para proyecto

--- Ses06/bulma-dashboard/webpack.common.js
+++ Ses06/bulma-dashboard/webpack.common.js
@@ -11,7 +11,8 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({template: './src/index.html'})
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new HtmlWebpackPlugin({template: './src/dashboard.html', filename:'dashboard.html'})
   ],
   module:{
     rules:[
